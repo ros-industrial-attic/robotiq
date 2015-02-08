2586c4b4adba3da0ba1c99f5a334296449caaf32
Jonathan Meyer <jonathan.meyer@swri.org>
2015-02-08 16:55:22 -0600

    Added ethercat client code. I don't like the structure of the code atm and will work to fix it shortly

---
+++ robotiq_c_model_control/src/robotiq_c_model_control/c_model_ethercat_node.cpp
@@ -0,0 +1,69 @@
+#include "ros/ros.h"
+
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+
+#include "robotiq_c_model_control/c_model_ethercat_client.h"
+#include <robotiq_c_model_control/CModel_robot_input.h>
+#include "robotiq_ethercat/ethercat_manager.h"
+
+
+/*
+  Note that this code currently works only to control ONE CModel gripper
+  attached to ONE network interface. If you want to add more grippers
+  to the same network, you'll need to edit the source file.
+*/
+
+// sudo setcap cap_net_raw+ep <filename>
+
+
+void changeCallback(robotiq_c_model_control::CModelEtherCatClient& client,
+                    const robotiq_c_model_control::CModelEtherCatClient::GripperOutput::ConstPtr& msg)
+{
+  client.writeOutputs(*msg);
+}
+
+
+int main(int argc, char** argv)
+{
+  using robotiq_ethercat::EtherCatManager;
+  using robotiq_c_model_control::CModelEtherCatClient;
+
+  typedef CModelEtherCatClient::GripperOutput GripperOutput;
+  typedef CModelEtherCatClient::GripperInput GripperInput;
+
+  ros::init(argc, argv, "robotiqCModel");
+  
+  ros::NodeHandle nh ("~");
+
+  // Parameter names
+  std::string ifname;
+  int slave_no;
+
+  nh.param<std::string>("ifname", ifname, "eth0");
+  nh.param<int>("slave_number", slave_no, 1);
+
+  // Start ethercat manager
+  EtherCatManager manager(ifname);
+  // register client 
+  CModelEtherCatClient client(manager, slave_no);
+
+  // Sorry for the indentation, trying to keep it under 100 chars
+  ros::Subscriber sub = 
+        nh.subscribe<GripperOutput>("/CModelRobotOutput", 1,
+                                    boost::bind(changeCallback, boost::ref(client), _1));
+
+  ros::Publisher pub = nh.advertise<GripperInput>("/CModelRobotInput", 100);
+
+  ros::Rate rate(10); // 10 Hz
+
+  while (ros::ok()) 
+  {
+    CModelEtherCatClient::GripperInput input = client.readInputs();
+    pub.publish(input);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
