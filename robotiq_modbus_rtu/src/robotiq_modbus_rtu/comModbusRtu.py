485506410c25245ef9dbed17d06b19c7dbbe7869
nicolaslauzier <nicolas@robotiq.com>
2015-02-25 08:37:42 -0500

    Increase of timeout value for Modbus RTU client
    Required to increase stability when using USB converters

--- robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
+++ robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
@@ -53,7 +53,7 @@ class communication:
       
    def connectToDevice(self, device):
       """Connection to the client - the method takes the IP address (as a string, e.g. '192.168.1.11') as an argument."""
-      self.client = ModbusSerialClient(method='rtu',port=device,stopbits=1, bytesize=8, baudrate=115200, timeout=0.01)
+      self.client = ModbusSerialClient(method='rtu',port=device,stopbits=1, bytesize=8, baudrate=115200, timeout=0.2)
       if not self.client.connect():
           print "Unable to connect to %s" % device
           return False
