0e96c3ed0cdd694592055e671b2cefd15414b7f3
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2015-05-14 13:44:40 +0200

    modbus_(rtu|tcp): remove incorrect 'requires' key from setup args. Fix #10 and fix #22.
    The 'python-' prefix of the 'pymodbus' entry was not correct: only the debian
    package has that prefix. It caused the setup phase to fail, which made the
    robotiq docjob error out. It was also the reason for 'catkin_make install' not
    completing.
    
    instead of correcting the entry, the entire 'requires' key was removed from
    the setup args. Catkin packages should not be installed using the 'setup.py'
    file, and the dependency on 'python-modbus' is already expressed in the pkg
    manifest.

--- robotiq_modbus_rtu/setup.py
+++ robotiq_modbus_rtu/setup.py
@@ -7,7 +7,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['robotiq_modbus_rtu'],
     package_dir={'': 'src'},
-    requires=['rospy', 'python-pymodbus']
 )
 
 setup(**setup_args)
