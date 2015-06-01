10cb59ea339cbb4050d5e33629cfea5ea08a6d73
Jonathan Meyer <jonathan.meyer@swri.org>
2015-06-01 11:26:35 -0500

    Adjusted minimum effort for the C2 gripper based on feedback from fsuarez6 (from github)

--- robotiq_action_server/src/robotiq_c_model_action_server_node.cpp
+++ robotiq_action_server/src/robotiq_c_model_action_server_node.cpp
@@ -8,7 +8,7 @@ namespace
     robotiq_action_server::CModelGripperParams params;
     params.min_gap_ = -.017;
     params.max_gap_ = 0.085;
-    params.min_effort_ = 30.0; // This is a guess. Could not find data with quick search.
+    params.min_effort_ = 40.0; // This is a guess. Could not find data with quick search.
     params.max_effort_ = 100.0;
 
     return params;
