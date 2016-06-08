231a98ecc9f043fad2fc18936fc7c7b10b331954
Allison Thackston <allison.thackston@toyota.com>
2016-06-08 10:49:26 -0700

    fixed scissor joint offset and multiplier

--- robotiq_s_model_control/src/robotiq_s_model_control/s_model_api.cpp
+++ robotiq_s_model_control/src/robotiq_s_model_control/s_model_api.cpp
@@ -8,8 +8,8 @@ SModelAPI::SModelAPI(boost::shared_ptr<SModelClientBase> base)
 {
     pos_to_ticks_ = 200;
     pos_offset_ = 0;
-    sci_to_ticks_ = -1200;
-    sci_offset_ = 0.05333;
+    sci_to_ticks_ = -480;
+    sci_offset_ = 0.28125;
     vel_to_ticks_ = 2.94;
     vel_offset_ = 22;
     force_to_ticks_ = 5.7;
