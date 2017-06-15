fbfbed2207d6586cede5f7f3d4c6bed3990ac094
Marc Bestmann <0bestman@informatik.uni-hamburg.de>
2017-06-15 16:46:01 +0200

    Add rfcomm devices to the list of possible connections for the sensor.

--- robotiq_ft_sensor/src/rq_sensor_com.cpp
+++ robotiq_ft_sensor/src/rq_sensor_com.cpp
@@ -170,7 +170,8 @@ INT_8 rq_sensor_com()
     {
         //Look for a serial device
         if (strstr(entrydirectory->d_name, "ttyS") ||
-            strstr(entrydirectory->d_name, "ttyUSB"))
+            strstr(entrydirectory->d_name, "ttyUSB") ||
+            strstr(entrydirectory->d_name, "rfcomm"))
         {
             device_found = rq_com_identify_device(entrydirectory->d_name);
         }
