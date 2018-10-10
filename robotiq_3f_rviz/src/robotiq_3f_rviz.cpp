76ea4ea5a319501803d777f82f6c9169b853678b
Christian Rauch <Christian.Rauch@ed.ac.uk>
2018-10-10 20:56:01 +0100

    define RViz panel 'Robotiq3FingerPanel'

---
+++ robotiq_3f_rviz/src/robotiq_3f_rviz.cpp
@@ -0,0 +1,12 @@
+# include <rviz/panel.h>
+
+namespace robotiq_3f_rviz {
+
+class Robotiq3FingerPanel : public rviz::Panel {
+    //
+};
+
+}
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(robotiq_3f_rviz::Robotiq3FingerPanel,rviz::Panel )
