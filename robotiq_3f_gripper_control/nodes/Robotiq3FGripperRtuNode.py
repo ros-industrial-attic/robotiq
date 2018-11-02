c721ccae92e3d23f471caa8e11b425d0b214f25e
Henning Kayser <henningkayser@mail.de>
2018-11-02 17:15:28 +0100

    Adapt SModelRtuNode to modified identifier names and directories

--- robotiq_3f_gripper_control/nodes/Robotiq3FGripperRtuNode.py
+++ robotiq_3f_gripper_control/nodes/Robotiq3FGripperRtuNode.py
@@ -4,34 +4,34 @@
 ROS node for controling a Robotiq S-Model gripper using the Modbus RTU protocol.
 
 The script takes as an argument the device address of the gripper (e.g. /dev/ttyUSB0). Afterwards it acts like the
-SModelTcpNode.
+Robotiq3FGripperTcpNode.
 """
 
-import roslib; roslib.load_manifest('robotiq_s_model_control')
+import roslib; roslib.load_manifest('robotiq_3f_gripper_control')
 roslib.load_manifest('robotiq_modbus_tcp')
 import rospy
-import robotiq_s_model_control.baseSModel
+import robotiq_3f_gripper_control.baseRobotiq3FGripper
 import robotiq_modbus_rtu.comModbusRtu
 import os, sys
-from robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
-from robotiq_s_model_control.msg import _SModel_robot_output as outputMsg
+from robotiq_3f_gripper_control.msg import _Robotiq3FGripper_robot_input  as inputMsg
+from robotiq_3f_gripper_control.msg import _Robotiq3FGripper_robot_output as outputMsg
 
 
 def mainLoop(device):
-    # Gripper is a S-Model with a TCP connection
-    gripper = robotiq_s_model_control.baseSModel.robotiqBaseSModel()
+    # Gripper is a 3F gripper with a TCP connection
+    gripper = robotiq_3f_gripper_control.baseRobotiq3FGripper.robotiqbaseRobotiq3FGripper()
     gripper.client = robotiq_modbus_rtu.comModbusRtu.communication(retry=True)
 
     # We connect to the address received as an argument
     gripper.client.connectToDevice(device)
 
-    rospy.init_node('robotiqSModel')
+    rospy.init_node('robotiq3FGripper')
 
-    # The Gripper status is published on the topic named 'SModelRobotInput'
-    pub = rospy.Publisher('SModelRobotInput', inputMsg.SModel_robot_input, queue_size=1)
+    # The Gripper status is published on the topic named 'Robotiq3FGripperRobotInput'
+    pub = rospy.Publisher('Robotiq3FGripperRobotInput', inputMsg.Robotiq3FGripper_robot_input, queue_size=1)
 
-    # The Gripper command is received from the topic named 'SModelRobotOutput'
-    rospy.Subscriber('SModelRobotOutput', outputMsg.SModel_robot_output, gripper.refreshCommand)
+    # The Gripper command is received from the topic named 'Robotiq3FGripperRobotOutput'
+    rospy.Subscriber('Robotiq3FGripperRobotOutput', outputMsg.Robotiq3FGripper_robot_output, gripper.refreshCommand)
 
     # We loop
     while not rospy.is_shutdown():
