1e744f1870cdf9f8ba5a6ba244149a2d94d67e9d
John Stechschulte <john@picknik.ai>
2021-02-04 14:59:07 -0700

    more python3 updates

--- robotiq_modbus_tcp/src/robotiq_modbus_tcp/comModbusTcp.py
+++ robotiq_modbus_tcp/src/robotiq_modbus_tcp/comModbusTcp.py
@@ -36,7 +36,7 @@
 
 
 """@package docstring
-Module comModbusTcp: defines a class which communicates with Robotiq Grippers using the Modbus TCP protocol. 
+Module comModbusTcp: defines a class which communicates with Robotiq Grippers using the Modbus TCP protocol.
 
 The module depends on pymodbus (http://code.google.com/p/pymodbus/) for the Modbus TCP client.
 """
@@ -52,7 +52,7 @@ class communication:
    def __init__(self):
       self.client = None
       self.lock = threading.Lock()
-      
+
    def connectToDevice(self, address):
       """Connection to the client - the method takes the IP address (as a string, e.g. '192.168.1.11') as an argument."""
       self.client = ModbusTcpClient(address)
@@ -61,9 +61,9 @@ class communication:
       """Close connection"""
       self.client.close()
 
-   def sendCommand(self, data):   
+   def sendCommand(self, data):
       """Send a command to the Gripper - the method takes a list of uint8 as an argument. The meaning of each variable depends on the Gripper model (see support.robotiq.com for more details)"""
-      #make sure data has an even number of elements   
+      #make sure data has an even number of elements
       if(len(data) % 2 == 1):
          data.append(0)
 
@@ -71,7 +71,7 @@ class communication:
       message = []
 
       #Fill message by combining two bytes in one register
-      for i in range(0, len(data)/2):
+      for i in range(0, len(data)//2):
          message.append((data[2*i] << 8) + data[2*i+1])
 
       #To do!: Implement try/except
@@ -82,7 +82,7 @@ class communication:
       """Sends a request to read, wait for the response and returns the Gripper status. The method gets the number of bytes to read as an argument"""
       numRegs = int(ceil(numBytes/2.0))
 
-      #To do!: Implement try/except 
+      #To do!: Implement try/except
       #Get status from the device
       with self.lock:
          response = self.client.read_input_registers(0, numRegs)
@@ -94,6 +94,6 @@ class communication:
       for i in range(0, numRegs):
          output.append((response.getRegister(i) & 0xFF00) >> 8)
          output.append( response.getRegister(i) & 0x00FF)
-      
+
       #Output the result
       return output
