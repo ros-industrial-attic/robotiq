978c5600ff435a4f92887f5a931720135d17d0e8
Leo-Dan <leo-daniel.gosselin.1@ens.etsmtl.ca>
2022-04-26 22:38:32 -0400

    Button Y is for activating the gripper at startup of the node

--- robotiq_2f_gripper_control/nodes/joy_controller.py
+++ robotiq_2f_gripper_control/nodes/joy_controller.py
@@ -6,58 +6,46 @@ import rospy
 from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output  as outputMsg
 from sensor_msgs.msg import Joy
 
-
-def joy_listener():
-
-    # start node
-    rospy.init_node("joy_controller", anonymous=True)
-
-    # subscribe to joystick messages on topic "joy"
-    rospy.Subscriber("/joy", Joy, joy_callback, queue_size=1)
-
-    # keep node alive until stopped
-    rospy.spin()
-
-
-
 #GLOBAL TEMP
 command = outputMsg.Robotiq2FGripper_robot_output()
 
 
-# called when joy cmd_joy message is received
+def joy_listener():
+    global command
+    rospy.init_node("joy_controller", anonymous=True)
+    rospy.Subscriber("/joy", Joy, joy_callback, queue_size=1)
+    rospy.spin()
+
+
 def joy_callback(data):
     global command
     
     button_B = data.buttons[1]
     button_X = data.buttons[2]
+    button_Y = data.buttons[3]
 
-    button_back = data.buttons[6]
-    button_start = data.buttons[7]
+    if button_Y:
+        command = outputMsg.Robotiq2FGripper_robot_output()
+        command.rACT = 1  #activates the gripper 
+        command.rGTO = 1  #Go to resqueted position
+        command.rFR = 255 #maximum force
+        command.rSP = 255 #Maximum speed
     
-    #activate (droite du bouton Xbox)
-    if button_start:
-        command = outputMsg.Robotiq2FGripper_robot_output()
-        command.rACT = 1
-        command.rFR = 150
-        command.rGTO = 1
-        command.rSP = 255
-    #reset (gauche du bouton Xbox)
-    elif button_back:
-        command = outputMsg.Robotiq2FGripper_robot_output()
-    #ferme
     elif button_B:
-        command.rPR = 255
-    #ouvre
+        command.rPR = 255 #close
+    
     elif button_X:
-        command.rPR = 0
+        command.rPR = 0 #open
    
+    
     pub = rospy.Publisher('Robotiq2FGripperRobotOutput', outputMsg.Robotiq2FGripper_robot_output, queue_size=1)
     pub.publish(command)
 
 
 
 if __name__ == '__main__':
-    try:                      
+    try:
+                              
         joy_listener()
 
     except rospy.ROSInterruptException:
