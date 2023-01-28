781ece6353eb69655895cc08e8601796b9f0cd89
Jack Naylor <35677585+nackjaylor@users.noreply.github.com>
2023-01-28 15:24:27 +1100

    Update getters

--- robotiq_3f_gripper_articulated_gazebo_plugins/src/RobotiqHandPlugin.cpp
+++ robotiq_3f_gripper_articulated_gazebo_plugins/src/RobotiqHandPlugin.cpp
@@ -31,12 +31,11 @@ Due to necessity, I had to change the PID.hh file's definition from private memb
 I'm not sure exactly where the dependency chain includes PID.hh for the first time, so I've encapsulated all of the gazebo includes. Not pretty, but it works. If you're reading this and know of a better soln', feel free to change it.
 
 */
-#define private public 
+
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/Time.hh>
 #include <gazebo/physics/physics.hh>
 #include <robotiq_3f_gripper_articulated_gazebo_plugins/RobotiqHandPlugin.h>
-#undef private
 
 // Default topic names initialization.
 const std::string RobotiqHandPlugin::DefaultLeftTopicCommand  =
@@ -148,7 +147,7 @@ void RobotiqHandPlugin::Load(gazebo::physics::ModelPtr _parent,
     if (this->sdf->HasElement("kd_position"))
     {
       this->posePID[i].SetDGain(this->sdf->Get<double>("kd_position"));
-      std::cout << "dGain after overloading: " << this->posePID[i].dGain
+      std::cout << "dGain after overloading: " << this->posePID[i].GetDGain()
                 << std::endl;
     }
 
@@ -227,13 +226,13 @@ void RobotiqHandPlugin::Load(gazebo::physics::ModelPtr _parent,
   {
     gzlog << "Position PID parameters for joint ["
           << this->fingerJoints[i]->GetName() << "]:"     << std::endl
-          << "\tKP: "     << this->posePID[i].pGain  << std::endl
-          << "\tKI: "     << this->posePID[i].iGain  << std::endl
-          << "\tKD: "     << this->posePID[i].dGain  << std::endl
-          << "\tIMin: "   << this->posePID[i].iMin   << std::endl
-          << "\tIMax: "   << this->posePID[i].iMax   << std::endl
-          << "\tCmdMin: " << this->posePID[i].cmdMin << std::endl
-          << "\tCmdMax: " << this->posePID[i].cmdMax << std::endl
+          << "\tKP: "     << this->posePID[i].GetPGain()  << std::endl
+          << "\tKI: "     << this->posePID[i].GetIGain()  << std::endl
+          << "\tKD: "     << this->posePID[i].GetDGain()  << std::endl
+          << "\tIMin: "   << this->posePID[i].GetIMin()   << std::endl
+          << "\tIMax: "   << this->posePID[i].GetIMax()   << std::endl
+          << "\tCmdMin: " << this->posePID[i].GetCmdMin() << std::endl
+          << "\tCmdMax: " << this->posePID[i].GetCmdMax() << std::endl
           << std::endl;
   }
   gzlog << "Topic for sending hand commands: ["   << controlTopicName
